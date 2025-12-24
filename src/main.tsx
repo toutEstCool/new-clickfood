7d709526d1a2826e05249c7a9035268018a5516b
toutEstCool <danilapanarin96@gmail.com>
2025-12-24 12:58:49 +0600

    totod: added todos list

--- src/main.tsx
+++ src/main.tsx
@@ -11,3 +11,13 @@ createRoot(document.getElementById('root')!).render(
     </ErrorBoundary>
   </StrictMode>
 )
+
+/* Todo 23.12*/
+// 1. Миграция Vite 7.2 / React Compiler * Version: "Release Candidate" [done 23.12.2025]
+// 2. Eslint, prettier [done 23.12.2025]
+// 3. Рефактор ErrorBoundary, ErrorTestComponent [done 23.12.2025]
+// 4. Рефактор роутинга [done 23.12.2025]
+
+/* Todo 24.12*/
+// 1. Precommit хуки [24.12.2025]
+// 2. Обновить TG-SDK [24.12.2025]
